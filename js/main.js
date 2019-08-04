6fc0b897bfbb3f22d0a626c7945f41358a7acc69
Victor Mauricio Restrepo Ossa <victor.restrepoo@upb.edu.co>
2019-08-04 15:28:25 -0500

    Se finaliza totalmente el desarrollo de la pagina version movil

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     // Funcion que se ejecuta al hacer click sobre una imagen
-    $("#eventosUbicacion img").click(function(){
+    $("#eventosUbicacion img").click(function () {
         // Posicionamos las capas
-        $('#background').css('height',$(document).height());
-        $('#preview').css('top',(($(window).height()/2) - ($('#preview').height()/2) + $(document).scrollTop()));
-        $('#preview').css('left', ($(document).width()/2) - ($('#preview').width()/2));
-        // Cargamos la imagen en la capa grande
-        $('#content').html("<img src='"+$(this).attr("src")+"'>");
-        // Mostramos las capas
-        $('#preview').fadeIn();
-        $('#background').fadeIn();
-    });
-    
+        if (screen.width > 720) {
+            $('#background').css('height', $(document).height());
+            $('#preview').css('top', (($(window).height() / 2) - ($('#preview').height() / 2) + $(document).scrollTop()));
+            $('#preview').css('left', ($(document).width() / 2) - ($('#preview').width() / 2));
+            // Cargamos la imagen en la capa grande
+            $('#content').html("<img src='" + $(this).attr("src") + "'>");
+            // Mostramos las capas
+            $('#preview').fadeIn();
+            $('#background').fadeIn();
+        }});
+
     // Cerramos las capas al pulsar sobre el fondo
-    $("#background").click(function(){
+    $("#background").click(function () {
         $('#background').fadeOut();
         $('#preview').fadeOut();
     });
     // Cerramos las capas al pulsar sobre la cruz
-    $("#close").click(function(){
+    $("#close").click(function () {
         $('#background').fadeOut();
         $('#preview').fadeOut();
     });
+
 });
